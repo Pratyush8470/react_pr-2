fde41383c82ff5b70cb88fcf3be8a2385abf1077
Pratyush8470 <pratyushbhatiya106@gmail.com>
2024-11-22 16:52:56 +0530

    pswd gnrtr

--- README.md
+++ README.md
@@ -1,8 +1,73 @@
-# React + Vite
+# Password Generator
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A sleek and user-friendly password generator built with ReactJS, Vite, and Tailwind CSS. This application allows users to generate secure, customizable passwords with just a few clicks.
 
-Currently, two official plugins are available:
+---
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Features
+
+- **Customizable Password Options**: 
+  - Select password length.
+  - Include/exclude numbers, and special characters.
+- **Copy to Clipboard**: Easily copy the generated password.
+
+---
+
+## Technologies Used
+
+- **ReactJS**: For building the interactive user interface.
+- **Vite**: For fast and efficient development and build process.
+- **Tailwind CSS**: For modern and styling.
+
+---
+
+## Installation
+
+Follow these steps to set up the project locally:
+
+## Clone the repository
+```bash
+git clone https://github.com/Pratyush8470/react_pr-2.git
+```
+
+## Navigate to the project directory
+```bash
+cd password-genera tor
+```
+
+## Install dependencies
+```bash
+npm install
+```
+## Start the development server
+```bash
+npm run dev
+```
+
+## Usage
+1. Open the app in your browser.
+2. Adjust settings like password length, character types, etc.
+3. Use the "Copy" button to copy the generated password to your clipboard.
+
+password-generator
+```bash/
+├── public/                   
+│   ├── vite.svg               
+│   ├── robots.txt           
+├── src/                      
+│   ├── assets/                
+│   ├── components/           
+│   │   ├── PasswordInput.jsx 
+│   │   ├── Controls.jsx      
+│   │   ├── Header.jsx        
+│   ├── App.jsx              
+│   ├── main.jsx          
+│   ├── App.css             
+│   ├── index.css             
+├── .gitignore                
+├── index.html               
+├── package.json              
+├── postcss.config.js        
+├── vite.config.js            
+├── README.md   
+```              
--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,98 @@
-import { useState } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const [length, setLength] = useState(6)
+  const [password, setPassword] = useState('')
+  const [number, setnumber] = useState(false)
+  const [character, setcharacter] = useState(false)
+
+  const pswd = useCallback(() => {
+    let pass = ''
+    let str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    if (number) str += '0123456789'
+    if (character) str += '!@#$%^&*()'
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+    setPassword(pass)
+  },[number, character, length, setPassword])
+
+  const ref= useRef(null)
+
+const copy = useCallback(() => {
+  ref.current.select()
+  navigator.clipboard.writeText(ref.current.value)
+},[password])
+
+  useEffect(() => {
+    pswd()
+  }, [number, character, length, pswd])
 
   return (
     <>
-      <div className="flex align-center justify-center py-40">
-        <div class="form w-6/12 h-40">
-          <input placeholder="Email Address" class="input" type="text" />
-          <div class="btn mb-[5px]">COPY TEXT</div>
-          <div className="wrap flex w-full h-full bg-white rounded-2xl overflow-hidden">
-            <div className="w-1/2 border-r border-[#8a8989] h-full flex justify-center align-middle bg-[#e07a5f]">
-              <label class="slider">
-                <input type="range" min="6" max="100" class="level" />
+      <div className='flex align-center justify-center py-40'>
+        <div class='form w-6/12 h-40'>
+          <input value={password} ref={ref} class='input' type='text' />
+          <div class='btn mb-[5px]' onClick={copy}>COPY TEXT</div>
+          <div className='wrap flex w-full h-full bg-white rounded-2xl overflow-hidden'>
+            <div className='w-1/2 border-r border-[#8a8989] h-full flex justify-center align-middle bg-[#e07a5f]'>
+              <label class='slider'>
+                <input
+                  type='range'
+                  value={length}
+                  min='6'
+                  max='20'
+                  class='level'
+                  onChange={e => setLength(e.target.value)}
+                />
+                <span className='text-[#f6eca9] font-bold mr-3'>{length}</span>
               </label>
             </div>
-            <div className="w-1/2 h-full flex justify-evenly align-middle bg-[#e07a5f]">
-              <label
-                for="hr"
-                class="flex flex-row items-center gap-2.5 cursor-pointer dark:text-white light:text-black"
-              >
-                <input id="hr" type="checkbox" class="peer hidden" />
-                <div
-                  for="hr"
-                  class="h-5 w-5 flex rounded-md border border-[#a2a1a833] light:bg-[#e8e8e8] dark:bg-[#212121] peer-checked:bg-[#f7f7f7] transition"
-                >
-                  <svg
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    class="w-5 h-5 light:stroke-[#e8e8e8] dark:stroke-[#212121]"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M4 12.6111L8.92308 17.5L20 6.5"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                  </svg>
-                </div>
-                <span className='text-[#f6eca9] font-bold'>CHARACTERS</span>
-              </label>
-              <label
-                for="hr"
-                class="flex flex-row items-center gap-2.5 cursor-pointer dark:text-white light:text-black"
-              >
-                <input id="hr" type="checkbox" class="peer hidden" />
-                <div
-                  for="hr"
-                  class="h-5 w-5 flex rounded-md border border-[#a2a1a833] light:bg-[#e8e8e8] dark:bg-[#212121] peer-checked:bg-[#f7f7f7] transition"
-                >
-                  <svg
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    class="w-5 h-5 light:stroke-[#e8e8e8] dark:stroke-[#212121]"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M4 12.6111L8.92308 17.5L20 6.5"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                  </svg>
-                </div>
-                <span className='text-[#f6eca9] font-bold'>DIGTS</span>
-              </label>
-
+            <div className='w-1/2 h-full flex justify-evenly items-center bg-[#e07a5f]'>
+              <div class='checkbox-wrapper-46'>
+                <input
+                  type='checkbox'
+                  id='cbx-46'
+                  defaultChecked={number}
+                  onChange={() => setnumber(prev => !prev)}
+                  class='inp-cbx'
+                />
+                <label for='cbx-46' class='cbx'>
+                  <span>
+                    <svg viewBox='0 0 12 10' height='10px' width='12px'>
+                      <polyline points='1.5 6 4.5 9 10.5 1'></polyline>
+                    </svg>
+                  </span>
+                  <span className='font-bold uppercase text-[#f6eca9]'>
+                    digit
+                  </span>
+                </label>
+              </div>
+              <div class='checkbox-wrapper-46'>
+                <input
+                  type='checkbox'
+                  id='cbx-47'
+                  class='inp-cbx'
+                  defaultChecked={number}
+                  onChange={() => setcharacter(prev => !prev)}
+                />
+                <label for='cbx-47' class='cbx'>
+                  <span>
+                    <svg viewBox='0 0 12 10' height='10px' width='12px'>
+                      <polyline points='1.5 6 4.5 9 10.5 1'></polyline>
+                    </svg>
+                  </span>
+                  <span className='font-bold uppercase text-[#f6eca9]'>
+                    character
+                  </span>
+                </label>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
     </>
   )
 }
